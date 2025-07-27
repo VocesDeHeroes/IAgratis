4d52103a8d5007ca53b343513a20ee179260934f
VocesDeHeroes <mir4naran@gmail.com>
2025-07-27 11:59:28 +0200

    firebase.js

--- descargador/firebase.js
+++ descargador/firebase.js
@@ -3,12 +3,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "1:XXXX:web:XXXX"
+  apiKey: "AIzaSyB6_o_Mm6QG7sHOD5zKITeVGhAtvKz25ZM",
+  authDomain: "iagratuita.firebaseapp.com",
+  projectId: "iagratuita",
+  storageBucket: "iagratuita.firebasestorage.app",
+  messagingSenderId: "133887632889",
+  appId: "1:133887632889:web:8c2136bae705aeb1a9c928",
+  measurementId: "G-KHFH4BPEQ0"
 };
 
 export const app = initializeApp(firebaseConfig);
